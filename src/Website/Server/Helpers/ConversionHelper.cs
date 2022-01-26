20570efb893409edf621588d99d3d53597e86d6b
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2022-01-26 19:42:28 +0100

    oops

--- src/Website/Server/Helpers/ConversionHelper.cs
+++ src/Website/Server/Helpers/ConversionHelper.cs
@@ -6,6 +6,8 @@ namespace Website.Server.Helpers
     {
         public static byte[] HexadecimalToBytes(string hex)
         {
+            hex = hex.Replace("-", string.Empty);
+
             int NumberChars = hex.Length;
             byte[] bytes = new byte[NumberChars / 2];
             for (int i = 0; i < NumberChars; i += 2)
