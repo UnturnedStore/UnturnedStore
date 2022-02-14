9d53b997f822e83c7ec4203902cc38338e2a272b
MCrow <michalwrona@hotmail.com>
2022-02-14 10:09:40 +0100

    Only enabled products

--- src/Database/procedures/GetHomeStatistics.sql
+++ src/Database/procedures/GetHomeStatistics.sql
@@ -4,7 +4,7 @@ BEGIN
 	SELECT
 		(SELECT COUNT(*) FROM dbo.Orders WHERE Status = 'Completed') AS SalesCount,
 		(SELECT COUNT(*) FROM dbo.Users) AS UsersCount,
-		(SELECT COUNT(*) FROM dbo.Products) AS ProductsCount;
+		(SELECT COUNT(*) FROM dbo.Products WHERE IsEnabled = 1 AND Status = 4) AS ProductsCount;
 
 	RETURN 0;
 END
