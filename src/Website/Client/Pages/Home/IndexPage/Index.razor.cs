8160858c4d32db2f125816f20e465308129e3e78
MCrow <michalwrona@hotmail.com>
2022-02-15 09:24:15 +0100

    Retore old index page

--- src/Website/Client/Pages/Home/IndexPage/Index.razor.cs
+++ src/Website/Client/Pages/Home/IndexPage/Index.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,6 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Website.Client.Services;
 using Website.Shared.Models.Database;
-using Website.Shared.Results;
 
 namespace Website.Client.Pages.Home.IndexPage
 {
@@ -15,14 +15,84 @@ namespace Website.Client.Pages.Home.IndexPage
     {
         [Inject]
         public HttpClient HttpClient { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public StorageService StorageService { get; set; }
 
-        public HomeStatisticsResult Statistics { get; set; }
         public IEnumerable<MProduct> Products { get; set; }
+
+        private IEnumerable<MProduct> SearchedProducts => Products
+            .Where(x => string.IsNullOrEmpty(searchCategory) || x.Category == searchCategory)
+            .Where(x => string.IsNullOrEmpty(searchString)
+            || x.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase)
+            || x.Seller.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase));
+
+        private IEnumerable<MProduct> OrderedProducts
+        {
+            get
+            {
+                switch (orderBy)
+                {
+                    case EOrderBy.MostDownloads:
+                        return SearchedProducts.OrderByDescending(x => x.TotalDownloadsCount);
+                    case EOrderBy.BestRated:
+                        return SearchedProducts.OrderByDescending(x => x.AverageRating).ThenByDescending(x => x.RatingsCount);
+                    case EOrderBy.PriceAsc:
+                        return SearchedProducts.OrderBy(x => x.Price);
+                    case EOrderBy.PriceDesc:
+                        return SearchedProducts.OrderByDescending(x => x.Price);
+                    default:
+                        return SearchedProducts.OrderByDescending(x => x.CreateDate);
+                }
+            }
+        }
+
+        private string searchString = string.Empty;
+        private string searchCategory = string.Empty;
+
+        private void ChangeCategory(string category)
+        {
+            searchCategory = category;
+        }
+
+        private string Active(string category)
+        {
+            if (searchCategory == category)
+                return "active";
+            return string.Empty;
+        }
+
+        private string GetCategoryIcon()
+        {
+            if (searchCategory == "Rocket Plugin")
+            {
+                return "fas fa-rocket";
+            }
+            else if (searchCategory == "OpenMod Plugin")
+            {
+                return "fas fa-plug";
+            }
+            else
+            {
+                return "far fa-folder";
+            }
+        }
 
         protected override async Task OnInitializedAsync()
         {
-            Statistics = await HttpClient.GetFromJsonAsync<HomeStatisticsResult>("api/home/statistics");
-            Products = await HttpClient.GetFromJsonAsync<MProduct[]>("api/home/promoted");
+            Products = await HttpClient.GetFromJsonAsync<MProduct[]>("api/products");
+        }
+
+        private EOrderBy orderBy = EOrderBy.Newest;
+
+        public enum EOrderBy
+        {
+            Newest,
+            MostDownloads,
+            BestRated,
+            PriceAsc,
+            PriceDesc
         }
     }
 }
