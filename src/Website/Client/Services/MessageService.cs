37cc6cd562e3324f0b87a9c1d15f14605d23657a
Gamingtoday093 <70017362+Gamingtoday093@users.noreply.github.com>
2022-05-30 23:08:41 +0200

    Updatedd MessageService

--- src/Website/Client/Services/MessageService.cs
+++ src/Website/Client/Services/MessageService.cs
@@ -30,107 +30,22 @@ namespace Website.Client.Services
         
         public List<MMessage> Messages { get; private set; }
         
+        public List<MMessage> NewMessages
+        {
+            get
+            {
+                return Messages.Where(m => m.CreateDate > userService.User.LastAccessedMessages || m.Replies.Any(mr => mr.CreateDate > userService.User.LastAccessedMessages)).ToList();
+            }
+        }
+        
         public async Task ReloadMessagesAsync()
         {
             Messages = await HttpClient.GetFromJsonAsync<List<MMessage>>("api/messages");
-            if (Carts == null)
-            {
-                Messages = new List<MMessage>(); // May Return here dont know
-            }
-
-            foreach (OrderParams orderParams in Carts.ToList())
-            {
-                orderParams.Seller = await httpClient.GetFromJsonAsync<Seller>($"api/users/{orderParams.SellerId}/seller");
-                foreach (OrderItemParams item in orderParams.Items.ToList())
-                {
-                    item.Product = await httpClient.GetFromJsonAsync<MProduct>("api/products/" + item.ProductId);
-
-                    if (item.Product.Customer != null)
-                    {
-                        await RemoveFromCartAsync(orderParams, item);
-                    }    
-                }
-            }
+            if (Messages == null)
+                Messages = new List<MMessage>();
 
             if (NavMenu != null)
                 NavMenu.Refresh();
-        }
-
-        public List<OrderParams> Carts { get; private set; }
-
-        public async Task ReloadCartAsync()
-        {
-            Carts = await storageService.GetSessionItemAsync<List<OrderParams>>("carts");
-            if (Carts == null)
-            {
-                Carts = new List<OrderParams>();
-            }
-
-            foreach (OrderParams orderParams in Carts.ToList())
-            {
-                orderParams.Seller = await httpClient.GetFromJsonAsync<Seller>($"api/users/{orderParams.SellerId}/seller");
-                foreach (OrderItemParams item in orderParams.Items.ToList())
-                {
-                    item.Product = await httpClient.GetFromJsonAsync<MProduct>("api/products/" + item.ProductId);
-
-                    if (item.Product.Customer != null)
-                    {
-                        await RemoveFromCartAsync(orderParams, item);
-                    }    
-                }
-            }
-
-            if (NavMenu != null)
-                NavMenu.Refresh();
-        }
-
-        public async Task UpdateCartAsync()
-        {            
-            await storageService.SetSessionItemAsync("carts", Carts);
-            if (NavMenu != null)
-                NavMenu.Refresh();
-        }
-
-        public OrderParams GetOrderParams(int sellerId)
-        {
-            return Carts.FirstOrDefault(x => x.SellerId == sellerId);
-        }
-
-        public async Task AddToCartAsync(OrderItemParams item)
-        {
-            OrderParams orderParams = Carts.FirstOrDefault(x => x.SellerId == item.Product.SellerId);
-            
-            if (orderParams == null)
-            {
-                orderParams = new OrderParams()
-                {
-                    SellerId = item.Product.SellerId,
-                    PaymentMethod = OrderConstants.Methods.PayPal,
-                    Seller = item.Product.Seller,
-                    Items = new List<OrderItemParams>()
-                };
-                Carts.Add(orderParams);
-            }
-
-            orderParams.Items.Add(item);            
-            await UpdateCartAsync();
-        }
-
-        public async Task RemoveFromCartAsync(OrderParams orderParams, OrderItemParams item)
-        {
-            orderParams.Items.Remove(item);
-            if (orderParams.Items.Count == 0)
-            {
-                Carts.Remove(orderParams);
-            }
-
-            await UpdateCartAsync();
-        }
-
-        public async Task RemoveCartAsync(OrderParams orderParams)
-        {
-            if (Carts.Remove(orderParams))
-                await UpdateCartAsync();
         }
     }
 }
