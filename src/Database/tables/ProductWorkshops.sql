b9d753c1221ed0337078b674d6595ff587fe584c
Gamingtoday093 <Gamingtoday093@gmail.com>
2022-07-21 15:37:12 +0200

    Forgot to change the name of the tables constraint

--- src/Database/tables/ProductWorkshops.sql
+++ src/Database/tables/ProductWorkshops.sql
@@ -2,7 +2,7 @@
 (
 	Id INT IDENTITY(1, 1) NOT NULL CONSTRAINT PK_ProductWorkshops PRIMARY KEY,
 	ProductId INT NOT NULL CONSTRAINT FK_ProductWorkshops_ProductId FOREIGN KEY REFERENCES dbo.Products(Id),
-	DatabaseFileId BIGINT NOT NULL CONSTRAINT UK_ProductWorkshops_WorkshopFileId DEFAULT 0,
+	DatabaseFileId BIGINT NOT NULL CONSTRAINT UK_ProductWorkshops_DatabaseFileId DEFAULT 0,
 	IsRequired BIT NOT NULL CONSTRAINT DF_ProductWorkshops_IsRequired DEFAULT 0,
 	UNIQUE (ProductId, DatabaseFileId)
 )
